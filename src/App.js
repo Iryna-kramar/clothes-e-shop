f2a5f8530631ae5c93cadc35784aa6344637c094
Iryna-kramar <kramar.twins@gmail.com>
2022-11-30 15:18:11 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-      
+    <div>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </Container>
     </div>
   );
