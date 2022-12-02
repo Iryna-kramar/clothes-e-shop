763d6b1b9e9bebf5991493433a5e3e0cb86448ce
Iryna-kramar <kramar.twins@gmail.com>
2022-12-02 18:03:35 +0200

    ShoppingCart refactoring

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
 import { Offcanvas, OffcanvasTitle, Stack } from "react-bootstrap";
 import { ShoppingCartContext } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import CartItem from "./CartItem";
+import storeItems from "../data/items.json";
 
 const ShoppingCart = ({ isOpen }) => {
   const { closeCart, cartItems } = useContext(ShoppingCartContext);
@@ -13,9 +15,20 @@ const ShoppingCart = ({ isOpen }) => {
         <OffcanvasTitle>Cart</OffcanvasTitle>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        <Stack gap={3}>{cartItems.map(item => (
-          <CartItem key={item.id} {...item}/>
-        ))}</Stack>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
+        </Stack>
       </Offcanvas.Body>
     </Offcanvas>
   );
